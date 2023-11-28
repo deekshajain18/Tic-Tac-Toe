f42605ad9a936d32de1acd6483eaf5befc3b87f1
deekshajain18 <deekshajain182003@gmail.com>
2023-11-28 19:40:14 +0530

    Add the code

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,102 @@
+def ConstBoard(board):
+    print("Current State of Board :  \n\n")
+    for i in range(0, 9):
+        if i > 0 and i % 3 == 0:
+            print("\n" + "---" * 2 + "---")
+        if board[i] == 0:
+            print("_", end=" ")
+        elif board[i] == -1:
+            print("X", end=" ")
+        elif board[i] == 1:
+            print("O", end=" ")
+        if (i + 1) % 3 != 0:
+            print("|", end=" ")
+    print("\n\n")
+
+
+def User1Turn(board):
+    pos = input("Enter X's position from [1,2,3....9]: ")
+    pos = int(pos)
+    if(board[pos-1] != 0):
+        print("Wrong Move")
+        exit(0)
+    board[pos-1] = -1
+
+def User2Turn(board):
+    pos = input("Enter X's position from [1,2,3....9]: ")
+    pos = int(pos)
+    if(board[pos-1] != 0):
+        print("Wrong Move")
+        exit(0)
+    board[pos-1] = -1
+
+def minmax(board, player):
+    x = analyzeboard(board)
+    if x != 0:
+        return x * player
+    pos = -1
+    value = -2
+    for i in range(0, 9):
+        if board[i] == 0:
+            board[i] = player
+            score = -minmax(board, player * -1)
+            board[i] = 0
+            if score > value:
+                value = score
+                pos = i
+    if pos == -1:
+        return 0
+    return value
+
+def CompTurn(board):
+    pos = -1
+    value = -2
+    for i in range(0, 9):
+        if board[i] == 0:
+            board[i] = 1
+            score = -minmax(board, -1)
+            board[i] = 0
+            if score > value:
+                value = score
+                pos = i
+    board[pos] = 1
+
+def analyzeboard(board):
+    cb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for i in range(0, 8):
+        if board[cb[i][0]] != 0 and board[cb[i][0]] == board[cb[i][1]] and board[cb[i][0]] == board[cb[i][2]]:
+            return board[cb[i][0]]
+    return 0
+
+def main():
+    choice = input("enter 1 for single player, 2 for multiplayer:")
+    choice = int(choice)
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    player = 1  # Initialize player
+
+    if choice == 1:
+        print("computer: O Vs. You: X")
+        player = input("Enter 1 for player 1 or 2 for player 2: ")
+        player = int(player)
+
+    for i in range(0, 9):
+        if analyzeboard(board) != 0:
+            break
+        if (i + player) % 2 == 0:
+            CompTurn(board)
+        else:
+            ConstBoard(board)
+            User1Turn(board) if player == 1 else User2Turn(board)
+
+    x = analyzeboard(board)
+    ConstBoard(board)
+
+    if x == 0:
+        print("Draw")
+    elif x == -1:
+        print("Player X wins!!! O Loses")
+    elif x == 1:
+        print("Player O wins!!! X Loses")
+
+if __name__ == "__main__":
+    main()
